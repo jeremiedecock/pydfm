e265a8cc1c8077879eb3c61f70e4773f7c298b53
Jeremie DECOCK <gremy@tuxfamily.org>
2010-12-14 21:45:53 +0100

    Add copyright notice

--- clonefinder.py
+++ clonefinder.py
@@ -3,6 +3,25 @@
 
 # Copyright (c) 2010 Jérémie DECOCK (http://www.jdhp.org)
 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+ 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
 
 # http://www.techsupportalert.com/best-free-duplicate-file-detector.htm
 
@@ -18,9 +37,9 @@ CHUNK_SIZE = 2**12
 def usage():
     """Print help message"""
 
-    print '''Usage : ./clonefinder [-h] DIRECTORY [DIRECTORY, ...]
+    print '''Usage : ./clonefinder [-h] DIRECTORY_1 [DIRECTORY_2, ...]
     
-    Find identical files in PATH.
+    Find duplicated files and directories in DIRECTORY_1, DIRECTORY_2, ...
 
     -h, --help
         display this help and exit
